5cdcc3106193d61b4d5c67de225ace41ed81f4f5
justin-hopper <justin.hopper@hp.com>
2012-12-19 14:19:54 -0800

    blueprint migrate-to-diskimage-builder
    Incorporated code review comments
    - removed dead files
    - added GUEST_USERNAME if needed to override (otherwise inherits host username)
    - cleaned up MySQL startup so that it does not start initially
    
    Added "elements" directory which contains the parts to build a reddwarf-guest and reddwarf-mysql image
    Modified "redstack" so that it installs diskimage-builder and copies elements to diskimage-builder directory (temp workaround)
    Modified "redstack.rc" to point to diskimage-builder repository
    Modified "functions_qemu" to replace ubuntu vm builder with diskimage-builder
    
    Change-Id: Ifd6a28d94b3cc8d4d8dc6792d5db48b6ec57004d

--- scripts/files/bootstrap_init-mysql.sh
+++ scripts/files/bootstrap_init-mysql.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 # Copies all the reddwarf code to the guest image
-sudo -u USERNAME rsync -e'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' -avz --exclude='.*' 10.0.0.1:PATH_REDDWARF/ /home/USERNAME/reddwarf
+sudo -u GUEST_USERNAME rsync -e'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' -avz --exclude='.*' HOST_USERNAME@10.0.0.1:PATH_REDDWARF/ /home/GUEST_USERNAME/reddwarf
 # Adds user to the sudoers file so they can do everything w/o a pass
-echo 'USERNAME ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+echo 'GUEST_USERNAME ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/rdguest-sudoers
+sudo chmod 0440 /etc/sudoers.d/rdguest-sudoers
 # Do an apt-get update since its SUPER slow first time
 apt-get update
 # Add extras which is _only_ in pip.....
