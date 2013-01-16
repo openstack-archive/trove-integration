86183d8985f8091f4d83d92f14c6d740dfe84ae5
justin-hopper <justin.hopper@hp.com>
2013-01-16 15:16:08 -0800

    Clean up scripts for guest image building
        creates sudoers files in tmp and sets perms before moving it to /etc/sudoers.d
        creates .ssh dir as root then changes perms and ownership to guest user
        creates symlinks to elements files rather than copying them to diskimage-builder
    
    Change-Id: Ib019e46107fab1362d57f5fec3cdb97214764614
    Fixes: bug/1100507

--- scripts/files/bootstrap_init-mysql.sh
+++ scripts/files/bootstrap_init-mysql.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 
+# Adds user to the sudoers file so they can do everything w/o a pass
+# Some binaries might be under /sbin or /usr/sbin, so make sure sudo will
+# see them by forcing PATH
+$TEMPFILE=`mktemp`
+echo "GUEST_USERNAME ALL=(ALL) NOPASSWD:ALL" > $TEMPFILE
+chmod 0440 $TEMPFILE
+sudo chown root:root $TEMPFILE
+sudo mv $TEMPFILE /etc/sudoers.d/60_reddwarf_guest
+
 # Copies all the reddwarf code to the guest image
 sudo -u GUEST_USERNAME rsync -e'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' -avz --exclude='.*' HOST_USERNAME@10.0.0.1:PATH_REDDWARF/ /home/GUEST_USERNAME/reddwarf
-# Adds user to the sudoers file so they can do everything w/o a pass
-echo 'GUEST_USERNAME ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/rdguest-sudoers
-sudo chmod 0440 /etc/sudoers.d/rdguest-sudoers
+
 # Do an apt-get update since its SUPER slow first time
 apt-get update
+
 # Add extras which is _only_ in pip.....
 pip install extras
+
 # Starts the reddwarf guestagent (using the upstart script)
 service reddwarf-guest start
