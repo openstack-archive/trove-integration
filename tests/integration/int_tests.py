90c4ed7e9114b3ba5ebed3a620d370a5bac1629a
Craig Vyvial <cp16net@gmail.com>
2014-03-10 14:17:40 -0500

    Configurations stored in the database
    This required an update the fake mode database and import the
    validation-rules.json file
    
    partially implements blueprint: configuration-parameters-in-db
    
    Change-Id: I183eedc5de42ad5c8474977c462bb6a69d6091b3

--- tests/integration/int_tests.py
+++ tests/integration/int_tests.py
@@ -92,7 +92,7 @@ def initialize_rdl_config(config_file):
         conf_file = conf.find_file(conf.api_paste_config)
     except RuntimeError as error:
         import traceback
-        print traceback.format_exc()
+        print(traceback.format_exc())
         sys.exit("ERROR: %s" % error)
 
 
@@ -154,6 +154,7 @@ def import_tests():
         from trove.tests.api import datastores
         from trove.tests.api.mgmt import accounts
         from trove.tests.api.mgmt import admin_required
+        from trove.tests.api.mgmt import configurations as mgmt_configurations
         from trove.tests.api.mgmt import hosts
         from trove.tests.api.mgmt import instances as mgmt_instances
         from trove.tests.api.mgmt import storage
@@ -175,6 +176,7 @@ def import_tests():
             backups.GROUP,
             configurations.GROUP,
             datastores.GROUP,
+            mgmt_configurations.GROUP,
             "dbaas.api.instances.actions.resize.instance",
             # TODO(SlickNik): The restart tests fail intermittently so pulling
             # them out of the blackbox group temporarily. Refer to Trove bug:
@@ -240,7 +242,6 @@ def import_tests():
         ]
         proboscis.register(groups=["couchbase"],
                            depends_on_groups=couchbase_groups)
-
 
         mongodb_groups = [
             "services.initialize",
